e4ff205d16992aa21829c3d0d9c5a96980735439
Quentin Ambard <quentin.ambard@gmail.com>
2022-12-22 15:30:13 +0100

    fix bug c360

---
+++ build-and-distribute.sh
@@ -0,0 +1,9 @@
+#increase the release (needs pip install bump)
+#bump
+#pachage
+rm -rf ./dist/*
+python3 setup.py clean --all bdist_wheel
+echo "Package built under dist/ - updating pypi with new version..."
+ls -alh ./dist
+twine upload dist/*
+echo "Upload ok - available as pip install dbdemos"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     #this will be the package name you will see, e.g. the output of 'conda list' in anaconda prompt
     name = 'dbdemos',
     #some version number you may wish to add - increment this after every update
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(exclude=["tests", "tests.*"]),
     setup_requires=["wheel"],
     include_package_data=True,
