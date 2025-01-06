63d753e78c319dbf3c59aabacabeaf13eb2b9077
Quentin Ambard <quentin.ambard@databricks.com>
2025-01-06 12:51:16 -0500

    fix bug last commit

--- setup.py
+++ setup.py
@@ -5,11 +5,7 @@ setup(
     #this will be the package name you will see, e.g. the output of 'conda list' in anaconda prompt
     name = 'dbdemos',
     #some version number you may wish to add - increment this after every update
-<<<<<<< Updated upstream
-    version='0.6.4',
-=======
     version='0.6.6',
->>>>>>> Stashed changes
     packages=find_packages(exclude=["tests", "tests.*"]),
     setup_requires=["wheel"],
     include_package_data=True,
