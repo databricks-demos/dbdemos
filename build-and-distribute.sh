8b4b6eff2d95f9460a75f3a0e0b81a41b7a108da
Quentin Ambard <quentin.ambard@databricks.com>
2025-02-07 13:15:05 -0500

    add back redirect to dbdemos.ai

--- build-and-distribute.sh
+++ build-and-distribute.sh
@@ -1,3 +1,6 @@
+#save current branch
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+echo "Current branch: $CURRENT_BRANCH"
 #increase the release (needs pip install bump)
 git checkout main || exit 1
 git pull || exit 1
@@ -11,3 +14,5 @@ ls -alh ./dist
 twine upload dist/*
 echo "Upload ok - available as pip install dbdemos"
 #TODO: should enforce commit before packaging and add a release tag in both dbdemos and dbdemos-notebooks
+#return to original branch
+git checkout $CURRENT_BRANCH || exit 1
